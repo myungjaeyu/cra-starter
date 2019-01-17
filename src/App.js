3036e0160fa64e09d2d440f20791c789ac66de90
myungjaeyu <rootcode10@gmail.com>
2019-01-17 12:18:18 +0900

    feat: Added sass.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
-import './App.css'
+import './App.scss'
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+
+                <span className="App__Title">
+                    cra-starter
+                </span>
 
             </div>
         )
