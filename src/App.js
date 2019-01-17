0e4e90bee4d4a1da63701c0e58678f173ada0a50
myungjaeyu <rootcode10@gmail.com>
2019-01-17 12:35:05 +0900

    feat: Added hash router.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react'
+import { Switch, Route, withRouter, Redirect } from 'react-router-dom'
 import './App.scss'
+
+import Main from './pages/Main'
+import Sub from './pages/Sub'
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
+            <div className='App'>
 
-                <span className="App__Title">
+                <span className='App__Title'>
                     cra-starter
                 </span>
+
+                <Switch>
+                    <Route exact path='/' component={ Main } />
+                    <Route exact path='/sub/:no' component={ Sub } />
+                    <Redirect to='/'/>
+                </Switch>
 
             </div>
         )
     }
 }
 
-export default App
+export default withRouter(App)
